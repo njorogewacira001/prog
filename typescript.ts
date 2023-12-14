24b305163ecf1b2d252e083b93afc7e7492787e3
Gavin <gavinwacira@gmail.com>
2023-12-14 16:11:56 +0300

    INITIAL COMMIT

--- typescript.ts
+++ typescript.ts
@@ -1,11 +1,78 @@
-// Empty array
-let numbers: number[] = [];
+// Basic types
+let name: string = "John Doe";
+let age: number = 25;
+let isStudent: boolean = true;
 
-// Array with initial values
-let fruits: string[] = ["apple", "banana", "orange"];
+// Array type
+let hobbies: string[] = ["Reading", "Coding", "Music"];
 
-// Array from another array
-let copiedFruits: string[] = fruits.slice();
+// Object type
+type User = {
+  name: string;
+  age: number;
+  hobbies: string[];
+};
 
-// Array with specific length
-let emptyStringArray: string[] = new Array(10);
+// Function type
+type AddNumbers = (a: number, b: number) => number;
+
+class
+ 
+Person
+ 
+{
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old!`);
+  }
+}
+
+let john = new Person("John Doe", 25);
+john.greet();
+
+// classes
+
+class
+ 
+Person
+ 
+{
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old!`);
+  }
+}
+
+let john = new Person("John Doe", 25);
+john.greet();
+
+
+// functions 
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+let sum = addNumbers(5, 10);
+console.log(`The sum is: ${sum}`);
+
+// Function with default parameter
+function printName(name: string = "John Doe"): void {
+  console.log(`Hello, ${name}!`);
+}
+
+printName("Jane"); // Prints: Hello, Jane!
+printName();
